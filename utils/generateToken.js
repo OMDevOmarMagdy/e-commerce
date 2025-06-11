80dee60d0ee3ac900b501046ababed6b711e3a6d
Omar Magdy <om8622145@gmail.com>
2025-06-12 01:40:46 +0300

    Just add a comment

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,11 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const generateToken = (user) => {
   return jwt.sign(
+    // comment
     { id: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '7d' }
+    { expiresIn: "7d" }
   );
 };
 
